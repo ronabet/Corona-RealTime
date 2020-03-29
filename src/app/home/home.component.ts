4e91d4a7aee99561107c4b9b433619ae1520bbd9
ronabet <ronabet456@gmail.com>
2020-03-29 17:49:26 +0300

    israel stats added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,7 +15,8 @@ export class HomeComponent implements OnInit {
   constructor(public api: ApiService) { }
 
   ngOnInit(): void {
-    this.getopen()
+    // this.getopen()
+    this.getIsrael()
   }
 
   
@@ -23,36 +24,50 @@ export class HomeComponent implements OnInit {
     this.api.getAllCountriesStat().subscribe((res: any) => {
       this.ResStat = res.countryitems;
       // console.log(this.ResStat["0"][1]);
-      this.StatsArray.push({
-        "title": this.ResStat["0"][1].title,
-        "total_cases": this.ResStat["0"][1].total_cases,
-        "total_recovered": this.ResStat["0"][1].total_recovered,
-        "total_deaths": this.ResStat["0"][1].total_deaths,
-        "total_new_cases_today": this.ResStat["0"][1].total_new_cases_today,
-        "total_new_deaths_today": this.ResStat["0"][1].total_new_deaths_today,
-        "total_active_cases": this.ResStat["0"][1].total_active_cases
-      }
-      )
-      console.log(this.StatsArray);
-      
-      
-      // this.ResStat.forEach((element: any) => {
-        // for(let i=0; i< this.ResStat.length; i++){
-        // this.StatsArray.push(
-        //   {
-        //     "country": this.ResStat[i].title,
-        //     "total_cases": this.ResStat[i].total_cases,
-        //     "total_recovered": this.ResStat[i].total_recovered,
-        //     "total_deaths": this.ResStat[i].total_deaths,
-        //     "total_new_cases_today": this.ResStat[i].total_new_cases_today,
-        //     "total_new_deaths_today": this.ResStat[i].total_new_deaths_today,
-        //     "total_active_cases": this.ResStat[i].total_active_cases
-        //    } as CountryStatisticModel
-        //    );
-        // }
-        })
-      }
+        this.StatsArray.push({
+          "title": this.ResStat["0"][1].title,
+          "total_cases": this.ResStat["0"][1].total_cases,
+          "total_recovered": this.ResStat["0"][1].total_recovered,
+          "total_deaths": this.ResStat["0"][1].total_deaths,
+          "total_new_cases_today": this.ResStat["0"][1].total_new_cases_today,
+          "total_new_deaths_today": this.ResStat["0"][1].total_new_deaths_today,
+          "total_active_cases": this.ResStat["0"][1].total_active_cases
+        }
+      ) 
+    console.log(this.StatsArray);
+    });
     }
+
+    getIsrael(){
+        this.api.getAllCountriesStat().subscribe((res: any) => {
+          this.ResStat = res.countryitems;
+          let index = 76;
+          // console.log(this.ResStat["0"][1]);
+          this.StatsArray.push({
+            "title": this.ResStat["0"][index].title,
+            "total_cases": this.ResStat["0"][index].total_cases,
+            "total_recovered": this.ResStat["0"][index].total_recovered,
+            "total_deaths": this.ResStat["0"][index].total_deaths,
+            "total_new_cases_today": this.ResStat["0"][index].total_new_cases_today,
+            "total_new_deaths_today": this.ResStat["0"][index].total_new_deaths_today,
+            "total_active_cases": this.ResStat["0"][index].total_active_cases
+        })
+          console.log(this.StatsArray);
+      })
+    }
+
+
+
+
+
+
+}
+
+
+
+    
+
+    
 
   
 
